f4d39b9a1febedb3a13fed4d4d0fc56a75d8ddaa
Steven <stzheng716@gmail.com>
2023-04-11 16:17:14 -0700

    add api request

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Let's get this party started!");
+"use strict";
+
+const API = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+const container = $("gifContainer");
+
+
+async function getGif(){
+    const searchTerm = $("#searchTerm").val();
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${API}`)
+    console.log(response.data)
+
+}   
+
+$("#submit").on("click", getGif)
