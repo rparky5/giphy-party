bde537867a88425d825baca19a8a190b5c2981a6
Ryan Park <ryrypark@gmail.com>
2023-04-11 17:08:12 -0700

    working gif generator

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 "use strict";
-
+// TODO: rename api variable
+// TODO: add base URL 
 const API = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
-const container = $("gifContainer");
+const container = $("#gifContainer");
 
+/** takes in user input and gets a gif URL based on the input
+ * and appends the gif to the DOM */
+async function getGif(evt){
+    evt.preventDefault();
 
-async function getGif(){
-    const searchTerm = $("#searchTerm").val();
-    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${API}`)
-    console.log(response.data)
+    const searchTerm = $("#searchTerm");
+    const response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm.val()}&api_key=${API}`)
+    const gif = response.data.data[0].images.downsized.url;
 
-}   
+    const newGif = $("<img>").attr("src", gif);
+    container.append(newGif);
 
+    searchTerm.val('');
+}
+
+/** empties the gif container when "remove gif" button is clicked*/
+function removeGif() {
+    container.empty();
+}
+
+$("#remove").on("click", removeGif);
 $("#submit").on("click", getGif)
